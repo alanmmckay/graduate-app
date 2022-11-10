1d6e5c487897cc1aef9714f436e6881137329f82
max-barten <maxbarten@uiowa.edu>
2022-11-10 11:14:17 -0600

    Created frame for application, and added rspec

---
+++ db/migrate/20221110153559_create_application.rb
@@ -0,0 +1,24 @@
+class CreateApplication < ActiveRecord::Migration
+  def change
+    create_table :applications do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :citizenship
+      t.string :gender
+      t.string :research_area
+      t.string :degree_obj
+      t.string :degree_obj_major
+      t.string :ug_inst
+      t.string :ug_inst_city
+      t.string :ug_gpa
+      t.string :ug_deg_earned
+      t.string :grad_inst
+      t.string :grad_inst_city
+      t.string :grad_gpa
+      t.string :grad_deg_earned
+      t.string :recommender_1
+      t.string :recommender_2
+      t.string :recommender_3
+    end
+  end
+end
