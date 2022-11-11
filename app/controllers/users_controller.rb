63b9e4bd079f2642bdbebacb7d75a35d88554ec1
Alan McKay <51932760+alanmmckay@users.noreply.github.com>
2022-11-11 14:52:37 -0600

    Implemented logic for insertion of new user based on name input and password input with verification. Need to implement regex and database validation for applicable values in these cases.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
 
   def user_params
-    params.require(:user).permit(:name)
+      params.require(:user).permit(:name, :password, :passwordV)
   end
+
   def index
     @users = User.all
   end
@@ -12,8 +13,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create!(user_params)
-    redirect_to users_path
+    info = user_params
+    if info[:password] == info[:passwordV]
+      @user = User.create!(name: info[:name], password: info[:password])
+      redirect_to users_path
+    else
+      puts 'fail'
+      flash[:notice] = "Passwords did not match"
+      redirect_to users_new_path
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20221107003055) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
