6849d87cd1651d7952d2b657dc2a3380862b6429
Alan McKay <51932760+alanmmckay@users.noreply.github.com>
2022-11-11 16:28:43 -0600

    Used migrations instead of hardcoding attributes into the schema/create tables entry. This is necessary as the databases that may be in place on other machines will need to have columns added.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221107003055) do
+ActiveRecord::Schema.define(version: 20221111222512) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "password"
   end
 
 end
