8c327a4ba9187050589dbd470eae404e0238f4a2
zalthof <zachary-althof@uiowa.edu>
2022-11-13 12:42:59 -0600

    updated routes in step definitions to reflect the landing page

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,9 +1,9 @@
 Given /^I am on the home page$/ do
-  visit users_path
+  visit 'users/login'
 end
 
 When /^I follow the link to the website$/ do
-  visit users_path
+  visit 'users/login'
 end
 
 Then /^I should see a landing page that says "(.*?)"$/ do |content|
