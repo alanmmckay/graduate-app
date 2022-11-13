1c9cc3fb6c4c41ad8baf62ed77ee6fbfd69c8ac7
zalthof <zachary-althof@uiowa.edu>
2022-11-13 12:37:01 -0600

    added relationship to users for users have one student, updated crud routes for student controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,39 @@
 class StudentsController < ApplicationController
-  def index
-    @students = Student.all
-    #TODO: Change this to return only students in the logged in department
+
+  before_filter :set_current_user, :only=> %w[show edit update delete]
+  def student_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:notice] = "Successfully created student account"
+      redirect_to 'users/login'
+    end
+  end
+  def show
+    @student = @current_user.student
+    #TODO: validate student not null -> faculty null / not
+  end
+
+  def edit
+    @student = @current_user.student
+    #TODO: validate student not null -> faculty null / not
+    #TODO: go to editable student info form
+  end
+
+  def update
+    @student = @current_user.student
+    #TODO: validate student not null -> faculty null / not
+    #TODO: go to editable student info form
+  end
+
+  def delete
+    @student = @current_user.student
+    @current_user.delete #TODO: ensure this will call user.delete and not student.dekete
+    @student.delete
+  end
+
+
 end
