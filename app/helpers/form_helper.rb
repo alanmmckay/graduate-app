8da9cc3ae4b2ba117d7f5fb92e1ad23760bb5eac
Alan McKay <51932760+alanmmckay@users.noreply.github.com>
2022-11-14 03:15:03 -0600

    create_form_input refactor. Also extended functiont to factor differing label styles

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,12 +1,12 @@
 # app/helpers/form_helper.rb
 module FormHelper
-  def create_form_input(helper_tag,object,method,label_text, options = {})
+  def create_form_input(helper_tag,object,method,label_text, options = {}, label_options = {})
     value = {}
     if flash[:info] and flash[:info][method]
       value = {:value => flash[:info][method]}
     end
 
-    label = label_tag (object.to_s + "_" +method.to_s).to_sym, label_text
+    label = label_tag (object.to_s + "_" +method.to_s).to_sym, label_text, label_options
     output = label + self.send(helper_tag,  object,  method, options.merge(value))
 
     #might negate this errors handling here
