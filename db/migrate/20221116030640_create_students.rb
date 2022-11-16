201b5591ff2d71e187aa970927ee748d5d2eceb1
Cole Garton <cole-garton@uiowa.edu>
2022-11-15 21:09:57 -0600

    new students db

--- db/migrate/20221116030640_create_students.rb
+++ db/migrate/20221116030640_create_students.rb
@@ -1,16 +1,10 @@
 class CreateStudents < ActiveRecord::Migration
   def up
     create_table :students do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :gender
-      t.references 'grad_application'
       t.references 'user'
-
-      t.timestamps null: false
+      t.string 'AppStatus'
     end
   end
-
   def down
     drop_table :students
   end
