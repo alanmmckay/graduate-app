bae414c09ec4f9fa414fc547cee521887ab6fd0e
max-barten <maxbarten@uiowa.edu>
2022-11-16 00:02:55 -0600

    Added view functionality that renders page again instead of redirecting if the fields are not valid

--- app/controllers/grad_application_controller.rb
+++ app/controllers/grad_application_controller.rb
@@ -14,35 +14,20 @@ class GradApplicationController < ApplicationController
   end
 
   def create
-    1.upto (10) do |i|
-      puts "%%"
-    end
     @params = grad_application_params
+    @grad_application = GradApplication.new(@params)
 
-    @grad_application = GradApplication.new(university: @params[:university], date: @params[:date], first_name: @params[:first_name], last_name: @params[:last_name],
-                                            citizenship: @params[:citizenship], gender: @params[:gender], research_area: @params[:research_area],
-                                            deg_obj: @params[:deg_obj], deg_obj_major: @params[:deg_obj_major], ug_inst: @params[:ug_inst],
-                                            ug_inst_city: @params[:ug_inst_city], ug_gpa: @params[:ug_gpa], ug_deg_earned: @params[:ug_deg_earned],
-                                            recommender_1: @params[:recommender_1])
-
-    print @grad_application
     if @grad_application.valid?
-      puts "VALID"
       @grad_application.save!
-      flash[:notice] = "Should be good"
-      redirect_to grad_application_faculty_test_path #(student home page)
-      flash[:notice] = "Should be good"
+      redirect_to ## grad_application_faculty_test_path #(student home page)
+      flash[:notice] = "Application has been successfully submitted"
     else
-      puts "NOT VALID"
       flash[:requirement] = @grad_application.errors
-      redirect_to grad_application_new_path #(student home page)
+      render grad_application_new_path
     end
   end
 
   def new
-    1.upto (10) do |i|
-      puts "@@"
-    end
     # default: render 'new' template
   end
 
@@ -54,7 +39,7 @@ class GradApplicationController < ApplicationController
   end
 
   def destroy # could be used for Withdrawing the GradApplication
-    @grad_application = GradApplication.find(params[:last_name])
+    @grad_application = GradApplication.find(params[:id])
     @grad_application.destroy
     # flash[:notice] = "Your GradApplications has been deleted."
     # redirect_to student home page
--- app/models/grad_application.rb
+++ app/models/grad_application.rb
@@ -1,10 +1,27 @@
 class GradApplication < ActiveRecord::Base
 
 
-  before_save
   validates :university, :date, :first_name, :last_name, :citizenship, :gender,
             :research_area, :deg_obj, :deg_obj_major, :ug_inst, :ug_inst_city,
             :ug_gpa, :ug_deg_earned, :recommender_1, presence: true
+  # validates :university, presence: true
+  # validates :date, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :gender, presence: true
+  # validates :research_area, presence: true
+  # validates :deg_obj, presence: true
+  # validates :deg_obj_major, presence: true
+  # validates :ug_inst, presence: true
+  # validates :ug_inst_city, presence: true
+  # validates :ug_gpa, presence: true
+  # validates :ug_deg_earned, presence: true
+  # validates :recommender_1, presence: true
+
+
+
+
+
 
 
 
