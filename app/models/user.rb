c1aa9ff512f72c11290cb7e25573824fe3e9cdf0
Alan McKay <51932760+alanmmckay@users.noreply.github.com>
2022-11-26 19:38:48 -0600

    bcrypt is now implemented. Resources used: https://hackernoon.com/authentication-and-authorization-with-bcrypt-in-rails-mw1g3u3l'

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class EmailValidator < ActiveModel::Validator
 end
 
 class User < ActiveRecord::Base
+  has_secure_password
   validates :password, :fname, :lname, presence: true
   validates :password_confirmation, presence: true
   validates :password, confirmation: true
