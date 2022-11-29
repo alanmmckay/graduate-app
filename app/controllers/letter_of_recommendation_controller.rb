d52178ef6fb28c17b6e965265428c5e46fe1f154
zalthof <zachary-althof@uiowa.edu>
2022-11-29 14:54:41 -0600

    added create to letter of recommendation controller

--- app/controllers/letter_of_recommendation_controller.rb
+++ app/controllers/letter_of_recommendation_controller.rb
@@ -1,15 +1,30 @@
 class LetterOfRecommendationController < ApplicationController
 
   def letter_of_recommendation_params
-    params.require(:letter_of_recommendation).permit(:letter)
+    params.require(:letter_of_recommendation).permit(:recommender_email,:letter)
   end
 
   def edit
 
   end
 
+  def update
+
+  end
+
   def create
     @params = letter_of_recommendation_params
+    @params[:url] = Digest::SHA256.digest "#{current_user.email}#{@params[:recommender_email]}"
+
     @letter_of_recommendation = LetterOfRecommendation.new(@params)
+
+    if @letter_of_recommendation.valid?
+      @letter_of_recommendation.save!
+      redirect_to # letter submitted view
+      flash[:notice] = "Letter of recommendation successfully submitted"
+    else
+      flash[:requirement] = @letter_of_recommendation.errors
+      render # where to go?
+    end
   end
 end
--- app/models/letter_of_recommendation.rb
+++ app/models/letter_of_recommendation.rb
@@ -1,6 +1,7 @@
 class LetterOfRecommendation < ActiveRecord::Base
   belongs_to :grad_application
   validates :recommender_email, presence: true, on: create
+  validates :url, uniqueness: true, presence: true, on: create
   after_initialize :init
 
   def init
