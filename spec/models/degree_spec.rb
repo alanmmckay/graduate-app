b5690bbc7c2bdf85495dbd4f64cfbd429ea3e870
Cole Garton <cole-garton@uiowa.edu>
2022-12-01 14:21:47 -0600

    Adding simple tests to degree_spec.rb

--- spec/models/degree_spec.rb
+++ spec/models/degree_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Degree, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "instance" do
+    before(:all) {
+      @valid_degree = Degree.new(name: 'UofI', city: 'Iowa City', major: "BME", degree_type: 'Bachelors of Science', gpa: '4.0');
+      @invalid_degree = Degree.new
+      @missing_one_degree = Degree.new(name: 'UofI', city: 'Iowa City', major: "BME", degree_type: 'Bachelors of Science')
+    }
+    describe "when valid degree instantiated" do
+      subject {@valid_degree}
+      it {should be_valid}
+    end
+    describe 'when invalid degree instantiated' do
+      subject {@invalid_degree}
+      it {should_not be_valid}
+    end
+    describe "has one of the attributes missing" do
+      subject {@missing_one_degree}
+      it {should_not be_valid}
+    end
+  end
 end
