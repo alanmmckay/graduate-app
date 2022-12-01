c97c597f2b372f01136db377ca0eb7053907c104
Cole Garton <cole-garton@uiowa.edu>
2022-12-01 14:20:35 -0600

    Fixing one last error in student_spec.rb

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe Student, type: :model do
   describe "instance" do
     before(:all) {
-      @valid_student = Student.new(gender: 'Male', citizenship: 'US Citizen');
+      @valid_student = Student.new(gender: 'Male', citizenship: 'US Citizen', address: '123 Sesame Street');
       @invalid_student = Student.new
       @missing_one_student = Student.new(gender: 'Male')
     }
-    describe "when valid user instantiated" do
+    describe "when valid student instantiated" do
       subject {@valid_student}
       it {should be_valid}
     end
-    describe 'when invalid user instantiated' do
+    describe 'when invalid student instantiated' do
       subject {@invalid_student}
       it {should_not be_valid}
     end
