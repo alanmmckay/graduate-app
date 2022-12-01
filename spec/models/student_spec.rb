3e5a23da11633eb34c189461252ab4889de34f83
Cole Garton <cole-garton@uiowa.edu>
2022-12-01 13:56:17 -0600

    Adding some simple tests to student_spec.rb, very similar to user_spec.rb

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "instance" do
+    before(:all) {
+      @valid_student = Student.new(gender: 'Male', citizenship: 'US Citizen');
+      @invalid_student = Student.new
+    }
+    describe "when valid user instantiated" do
+      subject {@valid_student}
+      it {should be_valid}
+    end
+    describe 'when invalid user instantiated' do
+      subject {@invalid_student}
+      it {should_not be_valid}
+    end
+    describe 'when valid user is instantiated using generate_user' do
+      subject {generate_student}
+      it {should be_valid}
+    end
+    describe "has one of the attributes missing" do
+      attributes = [:citizenship]
+      attributes.each do |attribute|
+        expect(generate_user( attribute )).to_not be_valid
+      end
+    end
+  end
 end
