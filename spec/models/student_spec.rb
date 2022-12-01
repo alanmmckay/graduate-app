162e843cfccb5adabef07770b729bc92eae4042c
Cole Garton <cole-garton@uiowa.edu>
2022-12-01 14:11:22 -0600

    Fixing an error in testing for student_spec.rb

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Student, type: :model do
     before(:all) {
       @valid_student = Student.new(gender: 'Male', citizenship: 'US Citizen');
       @invalid_student = Student.new
+      @missing_one_student = Student.new(gender: 'Male')
     }
     describe "when valid user instantiated" do
       subject {@valid_student}
@@ -14,15 +15,9 @@ RSpec.describe Student, type: :model do
       subject {@invalid_student}
       it {should_not be_valid}
     end
-    describe 'when valid user is instantiated using generate_user' do
-      subject {generate_student}
-      it {should be_valid}
-    end
     describe "has one of the attributes missing" do
-      attributes = [:citizenship]
-      attributes.each do |attribute|
-        expect(generate_user( attribute )).to_not be_valid
-      end
+      subject {@missing_one_student}
+      it {should_not be_valid}
     end
   end
 end
