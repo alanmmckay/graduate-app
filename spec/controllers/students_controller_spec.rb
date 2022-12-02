09be7e7cad52a3c3de1b8220c5cd09a80d4a47fa
Cole Garton <cole-garton@uiowa.edu>
2022-12-02 10:44:37 -0600

    This monkey patch is required to make controller specs run in ruby 2.6. source: https://github.com/rails/rails/issues/34790

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,10 +1,30 @@
 require 'spec_helper'
 require 'rails_helper'
 
+if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.6.0')
+  if Gem::Version.new(Rails.version) < Gem::Version.new('5.0.0')
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.7.0')
+          @mon_data = nil
+          @mon_data_owner_object_id = nil
+        else
+          @mon_mutex = nil
+          @mon_mutex_owner_object_id = nil
+        end
+        initialize
+      end
+    end
+  else
+    $stderr.puts "Monkeypatch for ActionController::TestResponse is no longer needed"
+  end
+end
+
 describe StudentsController do
   describe 'degree creation' do
     it 'should create a degree' do
-
+      expect(Degree).to receive(:new).with(name: 'UofI', city: 'Iowa City', major: "BME", degree_type: 'Bachelors of Science', gpa: '4.0')
+      post :degree_creation, {:degree_params => {name: 'UofI', city: 'Iowa City', major: "BME", degree_type: 'Bachelors of Science', gpa: '4.0'}}
     end
     it 'should link that degree to current user signed in' do
 
