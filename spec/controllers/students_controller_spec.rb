60150b393415b8226c323310f94e8fcf62540f3e
Cole Garton <cole-garton@uiowa.edu>
2022-12-02 10:43:24 -0600

    Test template

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,1 +1,33 @@
+require 'spec_helper'
+require 'rails_helper'
 
+describe StudentsController do
+  describe 'degree creation' do
+    it 'should create a degree' do
+
+    end
+    it 'should link that degree to current user signed in' do
+
+    end
+    it 'should redirect to the degree path' do
+
+    end
+    it 'should flash errors if there any' do
+
+    end
+  end
+  describe "update student" do
+    it 'should update student with info given' do
+
+    end
+    it 'should redirect to users path on update' do
+
+    end
+    it 'should stay on users edit path on fail' do
+
+    end
+    it 'should flash errors on fail' do
+
+    end
+  end
+end
