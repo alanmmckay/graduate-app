511ccc8d5c790aa4098ead03d5d0d6dbccef799b
Cole Garton <cole-garton@uiowa.edu>
2022-12-04 18:06:50 -0600

    Adding current progress of students_controller_spec.rb

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -23,31 +23,38 @@ end
 describe StudentsController do
   describe 'degree creation' do
     it 'should create a degree' do
+      controller.params[:degree] = {name: 'UofI', city: 'Iowa City', major: "BME", degree_type: 'Bachelors of Science', gpa: '4.0'}
+      allow_any_instance_of(StudentsController).to receive(:degree_creation)
       expect(Degree).to receive(:new).with(name: 'UofI', city: 'Iowa City', major: "BME", degree_type: 'Bachelors of Science', gpa: '4.0')
-      post :degree_creation, {:degree_params => {name: 'UofI', city: 'Iowa City', major: "BME", degree_type: 'Bachelors of Science', gpa: '4.0'}}
     end
     it 'should link that degree to current user signed in' do
-
+      pending
     end
-    it 'should redirect to the degree path' do
-
+    it 'should redirect to the degree path on a successful creation' do
+      allow(Degree).to receive(:new).with(name: 'UofI', city: 'Iowa City', major: "BME", degree_type: 'Bachelors of Science', gpa: '4.0')
+      post :degree_creation, {}
+      expect(response).to redirect_to '/students/degree'
     end
-    it 'should flash errors if there any' do
-
+    it 'should flash errors if there any on fail' do
+      fake_results = []
+      allow(Degree).to receive(:new).and_return(fake_results)
+      post :degree_creation, {}
+      expect(response).to redirect_to '/students/degree'
+      expect(flash[:notice]).to eq 'Must be filled'
     end
   end
   describe "update student" do
     it 'should update student with info given' do
-
+      pending
     end
     it 'should redirect to users path on update' do
-
+      pending
     end
     it 'should stay on users edit path on fail' do
-
+      pending
     end
     it 'should flash errors on fail' do
-
+      pending
     end
   end
 end
