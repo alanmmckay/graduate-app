b8865586f46c308e1528e466eb36635676499ae8
Alan McKay <51932760+alanmmckay@users.noreply.github.com>
2022-12-07 00:23:35 -0600

    grad_applications_controller is ready to accept params for all model types

--- app/controllers/grad_applications_controller.rb
+++ app/controllers/grad_applications_controller.rb
@@ -2,9 +2,20 @@ class GradApplicationsController < ApplicationController
 
   before_action :authorized
   def grad_application_params
-    params.require(:grad_applications).permit(:university, :research_area, :deg_obj, :deg_obj_major, :statement_of_purpose)
+    params.require(:grad_applications).permit(:university, :research_area, :deg_obj, :deg_obj_major, :statement_of_purpose, :recommender_1, :recommender_2, :recommender3, :fname, :lname, :address, :phone, :citizenship, :gender)
   end
 
+  def student_params
+    params.require(:student).permit(:grad_applications).permit(:university, :research_area, :deg_obj, :deg_obj_major, :statement_of_purpose, :recommender_1, :recommender_2, :recommender3, :fname, :lname, :address, :phone, :citizenship, :gender)
+  end
+
+  def user_params
+    params.require(:user).permit(:grad_applications).permit(:university, :research_area, :deg_obj, :deg_obj_major, :statement_of_purpose, :recommender_1, :recommender_2, :recommender3, :fname, :lname, :address, :phone, :citizenship, :gender)
+  end
+
+  def letter_params
+    params.require(:letter).permit(:grad_applications).permit(:university, :research_area, :deg_obj, :deg_obj_major, :statement_of_purpose, :recommender_1, :recommender_2, :recommender3, :fname, :lname, :address, :phone, :citizenship, :gender)
+  end
   def show
 
   end
