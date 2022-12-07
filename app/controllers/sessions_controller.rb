5613560fc4738242050a72edfa0994425d5d7a65
thomas12393 <tepawlowski@uiowa.edu>
2022-12-07 10:23:41 -0600

    added a function for the google verification to go to sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,21 @@
 class SessionsController < ApplicationController
   def create
     @user = User.authenticate(params[:email], params[:password])
-    auth = request.env["omniauth.auth"]
-    user = User.where(:provider => auth['provider'],
-                      :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
     if @user
       flash[:notice] = "You've signed in."
       session[:user_id] = @user.id
       redirect_to users_login_path
-    elsif user
-      reset_session
-      session[:user_id] = user.id
-      redirect_to root_url, :notice => 'Signed in!'
     else
       flash[:alert] = "There was a problem signing in. Please try again."
+      redirect_to users_path
+    end
+  end
+  def omniauth
+    user = user.from_omniauth(request.env['omniauth.auth'])
+    if user.valid?
+      session[:user_id] = user.id
+      redirect_to users_login_path
+    else
       redirect_to users_path
     end
   end
