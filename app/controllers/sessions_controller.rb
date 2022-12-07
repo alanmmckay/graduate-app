343a133c76ae1de742ecf896e46273ee4bd16ddb
thomas12393 <tepawlowski@uiowa.edu>
2022-12-07 06:30:39 -0600

    updated sessions controller to deal with the user if they are logging in with google

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,17 @@
 class SessionsController < ApplicationController
   def create
     @user = User.authenticate(params[:email], params[:password])
+    auth = request.env["omniauth.auth"]
+    user = User.where(:provider => auth['provider'],
+                      :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
     if @user
       flash[:notice] = "You've signed in."
       session[:user_id] = @user.id
       redirect_to users_login_path
+    elsif user
+      reset_session
+      session[:user_id] = user.id
+      redirect_to root_url, :notice => 'Signed in!'
     else
       flash[:alert] = "There was a problem signing in. Please try again."
       redirect_to users_path
@@ -15,5 +22,9 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:notice] = "You've signed out."
     redirect_to users_path
+  end
+
+  def failure
+    redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
   end
 end
