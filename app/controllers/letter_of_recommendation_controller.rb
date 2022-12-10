3b730351787df5f9b1d97d2f94d25ace15e1e5ec
zalthof <zachary-althof@uiowa.edu>
2022-12-10 12:59:28 -0600

    added scrub html to letter of recommendation controller

--- app/controllers/letter_of_recommendation_controller.rb
+++ app/controllers/letter_of_recommendation_controller.rb
@@ -4,13 +4,17 @@ class LetterOfRecommendationController < ApplicationController
     params.require(:letter_of_recommendation).permit(:email,:letter)
   end
 
+  def scrub_html(string)
+    string.gsub(/<div class="ql-editor" data-gramm="false" contenteditable="true">/,"<div class='ql-editor' data-gramm='false' contenteditable='false'>").gsub(/<\s*s\s*c\s*r\s*i\s*p\s*t\s*/,"").gsub(/<\s*\/\s*s\s*c\s*r\s*i\s*p\s*t\s*>/,"")
+  end
+
   def edit
     @letter_of_recommendation = LetterOfRecommendation.find_by_url(params[:id])
     @student = @letter_of_recommendation.grad_application.student
   end
 
   def update
-    letter = letter_of_recommendation_params[:letter]
+    letter = scrub_html(letter_of_recommendation_params[:letter])
     if params[:submit].to_bool
       LetterOfRecommendation.find_by_url(params[:id]).update(:letter => letter,:submitted => true)
       flash[:notice] = 'Letter of recommendation successfully submitted'
@@ -18,10 +22,13 @@ class LetterOfRecommendationController < ApplicationController
     else
       LetterOfRecommendation.find_by_url(params[:id]).update(:letter => letter,:submitted => false)
       flash[:notice] = 'Letter of recommendation successfully saved'
-      redirect_to letter_of_recommendation_update_path+'/'+params[:id].to_s
+      redirect_to letter_of_recommendation_show_path+'/'+params[:id].to_s
     end
   end
 
+  def show
+    @letter_of_recommendation = LetterOfRecommendation.find_by_url(params[:id])
+  end
   def self.create_letter(email, user)
     iterations = rand(1..10000)
     i = 0
@@ -34,7 +41,7 @@ class LetterOfRecommendationController < ApplicationController
     letter_of_recommendation = LetterOfRecommendation.new(:email => email, :url => url)
 
     if letter_of_recommendation.valid?
-      #Letter.with(user: user, url: url,email: email).email_recommender.deliver_now
+      Letter.with(user: user, url: url,email: email).email_recommender.deliver_now
     end
 
     letter_of_recommendation #this could have error messages associated; will need to check at higher scope
