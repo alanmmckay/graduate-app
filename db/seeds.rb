8ee4c862f17c2be8aa40ed7c9bf65719b8cf7001
Alan McKay <51932760+alanmmckay@users.noreply.github.com>
2022-12-10 11:24:21 -0600

    Cleaned up html, css styling, and bootstrap integration of the faculty home view. (Removed redundant styling and reordered html elements to ensure validity). As a side note, it's super interesting to me that form tags were used to display this information, wherein the form methods are gets.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#User.create!(email: "alan@test.com", password: "test", password_confirmation: "test", fname: "Alan", lname: "M")
+User.create!(email: "alan@test.com", password: "test", password_confirmation: "test", fname: "Alan", lname: "M")
 
 
 
 #TODO: Create Existing Faculty
 
-# user = User.new(email: "facultytestselt2022@gmail.com", password: "password1234", password_confirmation: "password1234", fname: "James", lname: "Ben", phone: "515-555-5555")
-# user.build_faculty(university: "University of Iowa")
-# user.save
+user = User.new(email: "facultytestselt2022@gmail.com", password: "password1234", password_confirmation: "password1234", fname: "James", lname: "Ben", phone: "515-555-5555")
+user.build_faculty(university: "University of Iowa")
+user.save
 
 
-# user = User.new(email: "facultytest2selt2022@test.com", password: "password1234", password_confirmation: "password1234", fname: "Brad", lname: "Clark", phone: "515-555-5555")
-# user.build_faculty(university: "University of Iowa")
-# user.save
+user = User.new(email: "facultytest2selt2022@test.com", password: "password1234", password_confirmation: "password1234", fname: "Brad", lname: "Clark", phone: "515-555-5555")
+user.build_faculty(university: "University of Iowa")
+user.save
 
